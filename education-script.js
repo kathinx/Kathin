ad64ef2e7766eb319161f536a77cfa97a405bea8
Kathin <120085951+kathinx@users.noreply.github.com>
2024-10-18 15:02:37 +0530

    Update education-script.js

--- education-script.js
+++ education-script.js
@@ -2,4 +2,4 @@ function toggleDropdown(id) {
     const dropdown = document.getElementById(id);
     dropdown.classList.toggle('active'); // Toggle the visibility of the dropdown
     dropdown.style.display = dropdown.style.display === 'block' ? 'none' : 'block'; // Show/Hide effect
-}
+} 

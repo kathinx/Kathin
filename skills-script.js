32f294a76546af07c29dc8aa73397dbf924d46d5
Kathin <120085951+kathinx@users.noreply.github.com>
2024-10-18 16:44:20 +0530

    Update skills-script.js

--- skills-script.js
+++ skills-script.js
@@ -1,9 +1,3 @@
-function toggleDropdown(id) {
-    const dropdown = document.getElementById(id);
-    dropdown.classList.toggle('active'); // Toggle the visibility of the dropdown
-    dropdown.style.display = dropdown.style.display === 'block' ? 'none' : 'block'; // Show/Hide effect
-}
-
 function showExplanation(id) {
     const explanation = document.getElementById(id);
     explanation.style.display = 'block'; // Show the explanation
